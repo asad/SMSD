52ae4c230d221d74ee91bff846bd63accce81a99
Syed Asad Rahman <s9asad@gmail.com>
2011-12-11 21:41:30 +0000

    imports fixed

--- src/org/openscience/smsd/algorithm/vflib/VF2MCS.java
+++ src/org/openscience/smsd/algorithm/vflib/VF2MCS.java
@@ -24,8 +24,6 @@ package org.openscience.smsd.algorithm.vflib;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
