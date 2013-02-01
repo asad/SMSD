47cc2bb453589e1516cc97f1105f17e8a6343824
Syed Asad Rahman <s9asad@gmail.com>
2013-02-01 11:23:11 +0000

    new comparator Signed-off-by:Syed Asad Rahman <s9asad@gmail.com>

---
+++ src/org/openscience/smsd/algorithm/vflib/SortOrder.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.openscience.smsd.algorithm.vflib;
+
+/**
+ *
+ * @author Asad
+ */
+public enum SortOrder {
+    ASCENDING, DESCENDING
+    
+}
