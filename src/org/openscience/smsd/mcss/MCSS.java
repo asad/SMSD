b05c5fa4742eca9c790f2a3cc9b1ac7b99c271aa
Syed Asad Rahman <s9asad@gmail.com>
2013-03-07 16:33:23 +0000

    thread debugger

--- src/org/openscience/smsd/mcss/MCSS.java
+++ src/org/openscience/smsd/mcss/MCSS.java
@@ -3,18 +3,22 @@
  *
  * Contact: cdk-devel@lists.sourceforge.net
  *
- * This program is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General
- * Public License as published by the Free Software Foundation; either version 2.1 of the License, or (at your option)
- * any later version. All we ask is that proper credit is given for our work, which includes - but is not limited to -
- * adding the above copyright notice to the beginning of your source code files, and to any copyright notice that you
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version. All we ask is that proper credit is given for our work,
+ * which includes - but is not limited to - adding the above copyright notice to
+ * the beginning of your source code files, and to any copyright notice that you
  * may distribute with programs based on this work.
  *
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
- * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
  *
- * You should have received a copy of the GNU Lesser General Public License along with this program; if not, write to
- * the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 package org.openscience.smsd.mcss;
 
@@ -40,13 +44,23 @@ final public class MCSS {
             LoggingToolFactory.createLoggingTool(MCSS.class);
     private final List<IAtomContainer> calculateMCSS;
 
+    /**
+     *
+     * @param jobList
+     * @param jobType
+     * @param numberOfThreads
+     */
     public MCSS(List<IAtomContainer> jobList, JobType jobType, int numberOfThreads) {
         int threadsAvailable = Runtime.getRuntime().availableProcessors() - 1;
+
+        logger.debug("Demand threads: " + numberOfThreads);
+        logger.debug(", Available threads: " + threadsAvailable);
         if (numberOfThreads > 0 && threadsAvailable >= numberOfThreads) {
             threadsAvailable = numberOfThreads;
         } else if (threadsAvailable <= 0) {
             threadsAvailable = 1;
         }
+        logger.debug(", Assigned threads: " + threadsAvailable + "\n");
         /*
          * Sort the molecules in the ascending order of atom size and atom type
          */
