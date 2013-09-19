f7df74d67066a520c01463d002e517557ac61399
Syed Asad Rahman <s9asad@gmail.com>
2013-09-19 10:42:40 +0100

    test cases Signed-off-by:Syed Asad Rahman <s9asad@gmail.com>

--- test/org/openscience/smsd/algorithm/mcsplus/McsplusSuite.java
+++ test/org/openscience/smsd/algorithm/mcsplus/McsplusSuite.java
@@ -1,25 +1,7 @@
-
-/* Copyright (C) 2009-2011 Syed Asad Rahman <asad@ebi.ac.uk>
- *
- * Contact: cdk-devel@lists.sourceforge.net
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public License
- * as published by the Free Software Foundation; either version 2.1
- * of the License, or (at your option) any later version.
- * All we ask is that proper credit is given for our work, which includes
- * - but is not limited to - adding the above copyright notice to the beginning
- * of your source code files, and to any copyright notice that you may distribute
- * with programs based on this work.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 package org.openscience.smsd.algorithm.mcsplus;
 
@@ -33,15 +15,11 @@ import org.junit.runners.Suite;
 /**
  * @author Syed Asad Rahman <asad@ebi.ac.uk>
  *
- * @cdk.module test-smsd
- * @cdk.require java1.6+
+ * @author Syed Asad Rahman <asad at ebi.ac.uk>
  */
 @RunWith(Suite.class)
 @Suite.SuiteClasses({org.openscience.smsd.algorithm.mcsplus.MCSPlusHandlerTest.class,
-    org.openscience.smsd.algorithm.mcsplus.MCSPlusTest.class,
-    org.openscience.smsd.algorithm.mcsplus.ExactMappingTest.class,
-    org.openscience.smsd.algorithm.mcsplus.GenerateCompatibilityGraphTest.class,
-    org.openscience.smsd.algorithm.mcsplus.BKKCKCFTest.class})
+    org.openscience.smsd.algorithm.mcsplus.IsomorphismMCSPlusTest.class})
 public class McsplusSuite {
 
     @BeforeClass
