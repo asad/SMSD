9b34336c76a4ea9526a7369fb72b40cc5c6b373e
Syed Asad Rahman <s9asad@gmail.com>
2014-02-06 11:44:06 +0000

    year updated

--- test/org/openscience/smsd/algorithm/vflib/Molecules.java
+++ test/org/openscience/smsd/algorithm/vflib/Molecules.java
@@ -1,6 +1,6 @@
 package org.openscience.smsd.algorithm.vflib;
 
-/* Copyright (C) 2009-2011  Syed Asad Rahman <asad@ebi.ac.uk>
+/* Copyright (C) 2009-2014  Syed Asad Rahman <asad@ebi.ac.uk>
  *
  * Contact: cdk-devel@lists.sourceforge.net
  *
